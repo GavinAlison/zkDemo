f0491dee3f61d0a8841654fa3fb5887a8058c0a5
921757697@qq.com <921757697@qq.com>
2019-06-17 23:08:55 +0800

    1

---
+++ zkjavaapi/readme.md
@@ -0,0 +1,9 @@
+原生的API
+
+
+zkClient 利用原生的API， 对其进行优化，
+主要解决了以下问题：重复注册watcher、session失效重连、异常处理。
+
+利用java的Listener来实现watcher机制
+
+
--- zkjavaapi/src/main/java/com/alison/zkjavaapi/zkClient/Create_Session.java
+++ zkjavaapi/src/main/java/com/alison/zkjavaapi/zkClient/Create_Session.java
@@ -2,6 +2,7 @@ package com.alison.zkjavaapi.zkClient;
 
 import com.alison.zkjavaapi.common.ZKConstant;
 import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.serialize.SerializableSerializer;
 
 
 /**
@@ -10,9 +11,10 @@ import org.I0Itec.zkclient.ZkClient;
  * @Version 1.0
  */
 public class Create_Session {
-//     使用ZkClient来创建一个ZooKeeper客户端
+    //     使用ZkClient来创建一个ZooKeeper客户端
     public static void main(String[] args) throws Exception {
-        ZkClient zkClient = new ZkClient(ZKConstant.CONNET_STR, ZKConstant.SESSION_TIMEOUT + 10000);
+//        ZkClient zkClient = new ZkClient(ZKConstant.CONNET_STR, ZKConstant.SESSION_TIMEOUT + 10000);
+        ZkClient zkClient = new ZkClient("192.168.56.103:2181", 10000, 10000, new SerializableSerializer());
         System.out.println("ZooKeeper session established.");
         System.out.println(zkClient);
     }
