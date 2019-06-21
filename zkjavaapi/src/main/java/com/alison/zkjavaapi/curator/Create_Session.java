da189c3888943ef43c915d1cc8271c69a2879d47
hy <921757697@qq.com>
2019-06-21 09:15:25 +0800

    auto commit

---
+++ zkjavaapi/src/main/java/com/alison/zkjavaapi/curator/Create_Session.java
@@ -0,0 +1,55 @@
+package com.alison.zkjavaapi.curator;
+
+import com.alison.zkjavaapi.common.ZKConstant;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+/**
+ * @Author: alison
+ * @Date: 2019/6/20
+ * @Version: 0.1
+ **/
+public class Create_Session {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    //使用curator来创建一个ZooKeeper客户端
+    public void create_session_sample() throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient(ZKConstant.CONNET_STR1, ZKConstant.SESSION_TIMEOUT, ZKConstant.CONNECT_TIMEOUT, retryPolicy);
+        client.start();
+        System.out.println(client);
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+    //使用Fluent风格的API接口来创建一个ZooKeeper客户端
+    public void create_Session_Sample_fluent() throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client =
+                CuratorFrameworkFactory.builder()
+                        .connectString(ZKConstant.CONNET_STR1)
+                        .sessionTimeoutMs(5000)
+                        .retryPolicy(retryPolicy)
+                        .build();
+        client.start();
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+    //使用curator来创建一个含隔离命名空间的ZooKeeper客户端
+    public void create_Session_Sample_With_Namespace() throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client =
+                CuratorFrameworkFactory.builder()
+                        .connectString(ZKConstant.CONNET_STR1)
+                        .sessionTimeoutMs(5000)
+                        .retryPolicy(retryPolicy)
+                        .namespace("base")
+                        .build();
+        client.start();
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
---
+++ zkjavaapi/src/main/java/com/alison/zkjavaapi/curator/Del_Data.java
@@ -0,0 +1,34 @@
+package com.alison.zkjavaapi.curator;
+
+import com.alison.zkjavaapi.common.ZKConstant;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+
+/**
+ * @Author: alison
+ * @Date: 2019/6/20
+ * @Version: 0.1
+ **/
+public class Del_Data {
+    static String path = "/zk-book/c1";
+    static CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString(ZKConstant.CONNET_STR1)
+            .sessionTimeoutMs(5000)
+            .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+            .build();
+
+    public static void main(String[] args) throws Exception {
+        client.start();
+        client.create()
+                .creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL)
+                .forPath(path, "init".getBytes());
+        Stat stat = new Stat();
+        client.getData().storingStatIn(stat).forPath(path);
+        client.delete().deletingChildrenIfNeeded()
+                .withVersion(stat.getVersion()).forPath(path);
+    }
+}
